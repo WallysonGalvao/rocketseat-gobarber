f2697eb2b5769206d207cd58e89e76358125d021
Galvao Da Cunha <wgalvao@indracompany.com>
2020-05-16 22:34:43 -0300

    mobile

---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import logo from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logo} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
