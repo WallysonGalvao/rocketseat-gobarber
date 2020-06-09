d29c81e042dcfb7ced3cf5bc0781106bddb723c9
Galvao Da Cunha <wgalvao@indracompany.com>
2020-06-09 17:55:42 -0300

    Reset de senha

--- backend/src/modules/users/repositories/fakes/FakeUserTokenRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUserTokenRepository.ts
@@ -16,6 +16,14 @@ class FakeUserTokenRepository implements IUserTokenRepository {
 
     return userToken;
   }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+
+    return userToken;
+  }
 }
 
 export default FakeUserTokenRepository;
---
+++ backend/src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ResetPasswordService from './ResetPasswordService';
+import FakeUserTokenRepository from '../repositories/fakes/FakeUserTokenRepository';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokenRepository: FakeUserTokenRepository;
+let resetPassword: ResetPasswordService;
+
+describe('ResetPasswordService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserTokenRepository = new FakeUserTokenRepository();
+    resetPassword = new ResetPasswordService(
+      fakeUsersRepository,
+      fakeUserTokenRepository,
+    );
+  });
+
+  it('should be able to reset the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokenRepository.generate(user.id);
+
+    await resetPassword.execute({
+      token,
+      password: '654321',
+    });
+
+    const updateUser = await fakeUsersRepository.findById(user.id);
+
+    expect(updateUser?.password).toBe('654321');
+  });
+});
