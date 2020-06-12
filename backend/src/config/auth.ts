f2f7e6bc5a0131153d975b6e6b76f95940c5edc5
Galvao Da Cunha <wgalvao@indracompany.com>
2020-06-12 16:47:44 -0300

    Variáveis de ambiente

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'gobarber',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -47,7 +47,7 @@ class SendForgotPasswordEmailService {
         file: forgotPaswordTemplate,
         variables: {
           name: user.name,
-          link: `http://localhost:3000/reset_password?token=${token}`,
+          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
         },
       },
     });
